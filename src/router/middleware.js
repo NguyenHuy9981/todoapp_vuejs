3627b8c5e671ec719298f9a6d81cd0ce7831e976
Huy <vuongsieu9981@gmail.com>
2023-03-04 14:14:38 +0700

    check permission

--- src/router/middleware.js
+++ src/router/middleware.js
@@ -3,12 +3,24 @@ import router from './index';
 
 let isInited = false;
 
+const authList = ['/login', '/register'];
+const whiteList = [...authList, '/'];
+
 router.beforeEach(async (to, from, next) => {
+  const { path } = to;
+
   if (store.getters.getUserAuthed && !isInited) {
     await store.dispatch('UserInfo');
     isInited = true;
   }
-  next();
+
+  if (store.getters.getUserAuthed && authList.includes(path)) {
+    next('/me');
+  } else if (!whiteList.includes(path) && !store.getters.getUserAuthed) {
+    next('/login');
+  } else {
+    next();
+  }
 });
 
 export default router;
