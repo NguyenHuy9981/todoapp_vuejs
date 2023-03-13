22a3361a1fc53f8d7429d606bc4497747259d0a0
Huy <vuongsieu9981@gmail.com>
2023-03-13 17:11:34 +0700

    scpoed css

--- src/views/ChangePass.vue
+++ src/views/ChangePass.vue
@@ -84,6 +84,6 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
   @import '@/styles/login.scss';
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -64,6 +64,6 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
   @import '@/styles/login.scss';
 </style>
