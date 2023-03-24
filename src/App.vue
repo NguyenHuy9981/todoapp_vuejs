545bb9001cbe161adb4ac545d50dbaaf5bbbddfb
Huy <vuongsieu9981@gmail.com>
2023-03-24 17:01:23 +0700

    delete css old

--- src/App.vue
+++ src/App.vue
@@ -64,34 +64,5 @@ export default {
 </script>
 
 <style lang="scss">
-.el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-  .el-col {
-    border-radius: 4px;
-  }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
