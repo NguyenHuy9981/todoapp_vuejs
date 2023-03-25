16fae72284afc8ef645405836eff6017a34b47e1
Huy <vuongsieu9981@gmail.com>
2023-03-25 09:21:31 +0700

    env

--- src/config/app.js
+++ src/config/app.js
@@ -1,3 +1,3 @@
-export const staticBase = 'http://localhost:3000/uploads';
-export const rootUrl = 'http://localhost:8080';
-export const apiBase = 'http://localhost:3000/api';
+export const staticBase = process.env.STATIC_URL || 'http://localhost:3000/uploads';
+export const rootUrl = process.env.ROOT_URL || 'http://localhost:8080';
+export const apiBase = process.env.API_URL || 'http://localhost:3000/api';
